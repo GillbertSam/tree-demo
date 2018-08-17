581e1f2adb816719124ff5ea7d2a86fb2fdc1a16
gillbert <miaokey@126.com>
2018-08-17 15:58:39 +0800

    1st commit

---
+++ treedemo/src/App.vue
@@ -0,0 +1,120 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+     <h2>Sunway</h2>
+     搜索：<input type="text" placeholder="请输入搜索类目" v-model="val" @keyup.enter="search()">
+     <button @click="search()" >search</button>
+     <p>{{msg}}</p>
+    <tree-demo @judge="showBox($event)" :datas="lists"></tree-demo>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld'
+import TreeDemo from "./components/TreeDemo";
+let lists = [
+  {
+    item: "a",
+    children: [{ name: "a1" }, { name: "a2" }, { name: "a3" }],
+    show: false
+  },
+  {
+    item: "b",
+    children: [{ name: "d1" }, { name: "e2" }, { name: "a3" }],
+    show: false
+  },
+  {
+    item: "c",
+    children: [{ name: "b1" }, { name: "c2" }, { name: "a3" }],
+    show: false
+  },
+  {
+    item: "d",
+    children: [{ name: "c1" }, { name: "b2" }, { name: "a3" }],
+    show: false
+  },
+  {
+    item: "e",
+    children: [{ name: "a1" }, { name: "d2" }, { name: "a3" }],
+    show: false
+  }
+];
+export default {
+  name: "App",
+  components: {
+    TreeDemo
+  },
+  data() {
+    return {
+      selectValue: "",
+      val: "",
+      lists: [],
+      msg:'a3为公共选项卡，搜索a1,b1--c2,d2排列组合选项只会展示一栏,输入a-e分类名，展示对应选项卡内容'
+    };
+  },
+  mounted() {
+    this.lists = lists;
+  },
+  methods: {
+    search: function() {
+      // console.log(this.val); //val 搜索值
+      if (!this.val && this.val == this.selectValue) {
+        return;
+      }
+      this.selectValue = this.val;
+      this.judgeMethod();
+    },
+    showBox: function(index) {
+      this.lists[index].show = !this.lists[index].show;
+    },
+    judgeMethod: function() {
+      //查询匹配数据
+      let tempArr = [];
+      for (let k = 0; k < this.lists.length; k++) {
+        console.log(this.lists[k]);
+        if (this.lists[k].item.indexOf(this.val) != -1) {
+          tempArr.push(k);
+        }
+        if (
+          this.lists[k].children &&
+          typeof this.lists[k].children == "object"
+        ) {
+          for (let j in this.lists[k].children) {
+            if (this.lists[k].children[j].name.indexOf(this.val) != -1) {
+              //name匹配
+              if (tempArr.indexOf(k) == -1) {
+                tempArr.push(k);
+              }
+            }
+          }
+        }
+      }
+      this.lists.forEach((v,i)=>{
+        v.show = false;
+      })
+      for (let i = 0; i < tempArr.length; i++) {
+        this.lists[tempArr[i]].show = true;
+      }
+    }
+  }
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  user-select: none;
+}
+input {
+  margin: 20px 0;
+}
+</style>
---
+++ treedemo/src/components/TreeDemo.vue
@@ -0,0 +1,82 @@
+<template>
+ <div id="tree-demo">
+     <!-- <ul v-if="lists.length > 0">
+         <li v-for="(list,index) in lists" :key="index">
+             <p>{{list.item}}</p>
+             <span>{{list.lists}}</span>
+             <TreeDemo v-if="list.children && list.children.length > 0">
+                 <li v-for="(list,index) in list.children" :key="index">
+                     <p>{{list.item}}</p>
+                 </li>
+             </TreeDemo>
+             <p v-else></p>
+         </li>
+     </ul> -->
+
+     <ul>
+         <li v-for="(list,index) in datas" :key="index">
+             <h5 @click="showBox(index)" class="title">{{list.item}}</h5>
+             <div class="box" :class="{show:list.show}">
+                 <p v-for="(item,index) in list.children" :key="index">
+                     {{item.name}}
+                 </p>
+             </div>
+         </li>
+     </ul>
+ </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  name: "TreeDemo",
+  props: ["select", "datas"],
+  data() {
+    return {
+      //   lists: [
+      //     { item: "a", children: [{ name: "1" }, { name: "2" }, { name: "3" }] ,show:false },
+      //     { item: "b", children: [{ name: "1" }, { name: "2" }, { name: "3" }] ,show:false },
+      //     { item: "c", children: [{ name: "1" }, { name: "2" }, { name: "3" }] ,show:false },
+      //     { item: "d", children: [{ name: "1" }, { name: "2" }, { name: "3" }] ,show:false},
+      //     { item: "e", children: [{ name: "1" }, { name: "2" }, { name: "3" }] ,show:false}
+      //   ]
+    };
+  },
+  components: {},
+  methods: {
+    showBox(index){
+        this.$emit('judge',index)
+    },
+  },
+  mounted() {}
+};
+</script>
+
+<style scoped>
+#tree-demo {
+  width: 15%;
+  border: 1px solid #ccc;
+  /* margin-left: 5px; */
+  margin: 20px auto;
+}
+#tree-demo ul {
+  list-style-type: none;
+}
+#tree-demo ul li {
+  border: 1px dashed #2dd;
+}
+.title {
+  font-size: 24px;
+}
+.box {
+  height: 0;
+  overflow: hidden;
+  transition: all 0.5s ease;
+  -webkit-transition: all 0.5s ease;
+}
+.box.show {
+  height: 60px;
+}
+.box p {
+  border: 1px dotted #e8e8e8;
+}
+</style>
